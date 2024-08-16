891b74be9c3a7ae46e10ae4552d975fa6078cb32
ClementLevi <1090708360@qq.com>
2024-08-17 01:31:10 +0800

    add: introduced "commander", "dotenv" modules and tested

--- server/index.js
+++ server/index.js
@@ -1,15 +1,44 @@
 const path = require("path");
+const fs = require("fs");
 
-// 加载命令行系统
-// Load Command Line System
-const {COMMAND_LINE, registerCommand,} = require(path.resolve("services/libs/shared/commandLine"))
-const COMMANDS_LIST = require(path.resolve("services/libs/commands"))
+function is_required() {
+    return "Application entry point can/should not be included!"
+}
+async function main() {
+    // 加载配置读取系统
+    // Load Config Loader
+    process.env.DOTENV_CONFIG_PATH = "./server/.env";
+    const dotenv = require("dotenv");
+    const CONFIG = dotenv.config().error ?
+        (() => {
+            throw dotenv.config().error;
+        })() :
+        dotenv.config().parsed;
 
-// 加载配置读取系统
-// Load Config Loader
-const CONFIG = require(path.resolve("services/libs/config"));
+    // 加载命令行系统
+    // Load Command Line System
+    const commander = require("commander");
+    const program = new commander.Command();
+    program
+        .name(CONFIG.SERVER_NAME)
+        .version(CONFIG.GAME_VERSION)
+        .description("这里需要本地化字符串，讲解服务器是干什么的")
+        .option("--test", "run unit tests")
+        // .option("-d, --debug", "Enable debugging")
+        // .option("-w, --windowed", "Run app in windowed mode")
+        .option(
+            "-n, --new",
+            "Create a new server. This will invoke world generator"
+        )
+        .option("-h, --help", program.help());
 
+    program.parse(process.argv);
+    const OPTS = program.opts();
+}
 
-
-
-console.log(CONFIG.serverSettings["server-name"]);
+if (require.main === module) {
+    main();
+} else {
+    let ret = is_required();
+    module.exports = ret;
+}
--- server/test/index.test.js
+++ server/test/index.test.js
@@ -1,11 +1,9 @@
 const expect = require("chai").expect;
 const index = require("../index");
 
-describe("Test Module", ()=>{
-    it("Main function functions", ()=>{
-        expect(index.test(2)).equal(114516)
+describe("Test Module", () => {
+    it("Main function should not be included", () => {
+        expect(index).equal("Application entry point can/should not be included!")
     })
-    it("Main function functions 2", ()=>{
-        expect(index.test(0)).equal(114514)
-    })
+
 })
