367651636a5e186d89e6c7267e5cd804ca172588
ClementLevi <1090708360@qq.com>
2025-08-24 23:43:04 +0800

    Refactor System class with improved structure and documentation
    Replaces the ISystem inheritance pattern with a single System class
    Adds comprehensive JSDoc type annotations for better code documentation
    Implements property getters/setters and improves constructor flexibility
    Enhances randomizeName method to support external name generators

--- server/services/GoFA-Remake/game_entities/atlas/system/System.js
+++ server/services/GoFA-Remake/game_entities/atlas/system/System.js
@@ -1,11 +1,14 @@
 const path = require("path");
 const uuid = require("uuid");
 const _ = require("lodash");
-const Pos2d = require(path.resolve(
-    "services/GoFA-Remake/game_entities/atlas/Pos/Pos2d.js"
-));
+const Pos2d = require(path.resolve(__dirname, "../Pos/Pos2d.js"));
 
 /**
+ * @module System
+ */
+
+/**
+ * @typedef {Object} SystemColor
  * @enum {number}
  * @readonly
  */
@@ -19,15 +22,38 @@ const ENUM_SYSTEM_COLOR = {
     WATER: 6,
     BLUE: 7,
     PURPLE: 8,
+    /**@default */
     WHITE: 9,
     BLACK_HOLE: 10,
 };
 
-class ISystem {
-    constructor(options) {
-        if (new.target === ISystem) {
-            throw new Error("Cannot instantiate ISystem directly");
-        }
+/**
+ * @typedef {import ("../Pos/Pos2d.js")} Pos2d
+ * ---
+ * @typedef {Object} SystemOptions
+ * @property {Pos2d} Pos
+ * @property {string} [name]
+ * @property {string} [appearance]
+ * @property {number} [size]
+ * @property {ENUM_SYSTEM_COLOR[keyof ENUM_SYSTEM_COLOR]} [color]
+ */
+
+class System {
+    static generateNames(count) {}
+    static ATTR_RANGE_LIMIT = Object.freeze({});
+    /**
+     * @overload
+     * @param {SystemOptions} options 系统选项对象
+     */
+    /**
+     * @overload
+     * @param {Pos2d} pos 系统位置
+     * @param {string} [name] 系统名称
+     * @param {string} [appearance] 系统外观
+     * @param {number} [size] 系统大小
+     * @param {ENUM_SYSTEM_COLOR[keyof ENUM_SYSTEM_COLOR]} [color] 系统颜色
+     */
+    constructor(options, name, appearance, size, color) {
         // if more than one argument is passed, pack them into an object as options
         if (arguments.length > 1) {
             if (arguments.length > 5) {
@@ -50,15 +76,13 @@ class ISystem {
         // Pos
         this.Pos = options.Pos;
         // Name
-        this.name = options.name
-            ? options.name
-            : "System_" + this.uuid.slice(0, 4);
-        // appearance
-        this.appearance = options.appearance ? options.appearance : "";
+        this.name = options.name ?? "System_" + this.uuid.slice(0, 4);
+        // appearance, for frontend rendering, no logic related to it.
+        // But it has to be static to store in db.
+        this.appearance = options.appearance ?? "";
         // size
-        this.size = options.size ? options.size : 1;
-        // color
-        this.color = options.color ? options.color : ENUM_SYSTEM_COLOR.WHITE;
+        this.size = options.size ?? 1;
+        this.color = options.color ?? ENUM_SYSTEM_COLOR.WHITE;
 
         // Dynamic properties 动态属性
         // Combat relative amount
@@ -70,94 +94,59 @@ class ISystem {
         this.is_name_modified = false;
         this.display_name = this.name;
     }
-}
-
-class System extends ISystem {
-
-    static generateNames(count){
-
-    }
-    constructor(options) {
-        super(options);
-        // if more than one argument is passed, pack them into an object as options
-        if (arguments.length > 1) {
-            if (arguments.length > 5) {
-                throw new Error("Too many arguments");
-            }
-            options = {
-                Pos: arguments[0],
-                name: arguments[1],
-                appearance: arguments[2],
-                size: arguments[3],
-                color: arguments[4],
-            };
-        }
-
-        // Constant properties 静态属性
-        // uuid
-        this.uuid = uuid.v4();
-        // Log generator options
-        this.options = JSON.stringify(options);
-        // Pos
-        this.Pos = options.Pos;
-        // Name
-        this.name = options.name
-            ? options.name
-            : "System_" + this.uuid.slice(0, 4);
-        // appearance
-        this.appearance = options.appearance ? options.appearance : "";
-        // size
-        this.size = options.size ? options.size : 1;
-        // color
-        this.color = options.color ? options.color : ENUM_SYSTEM_COLOR.WHITE;
-
-        // Dynamic properties 动态属性
-        // Combat relative amount
-        this.heat = 0; // calculated by recent combat activity
-        // Occupied by alliance
-        this.is_occupied = false;
-        this.controlled_by = null;
-        // Name can be modified when carnival is active
-        this.is_name_modified = false;
-        this.display_name = this.name;
-    }
-    getPos() {
-        return this.Pos;
-    }
-    getName() {
+    get Name() {
         return this.is_name_modified ? this.display_name : this.name;
     }
 
-    getHeat() {
+    set Name(name) {
+        this.display_name = name;
+    }
+
+    get Heat() {
         return this.heat;
     }
     getDistance(other_system) {
         return this.Pos.distanceTo(other_system.Pos);
     }
-    setName(name) {
-        this.display_name = name;
-    }
-    randomizeName() {
-        this.display_name = "System_" + uuid.v4().slice(0, 4);
+    /**
+     * @typedef {import ("./SystemNameGenerator.js")} InameGenerator
+     * ---
+     *
+     * @param {InameGenerator} nameGenerator
+     */
+    randomizeName(nameGenerator) {
+        if (!nameGenerator) {
+            this.display_name = "System_" + uuid.v4().slice(0, 4);
+        }
+        debugger
+        this.display_name = nameGenerator.pick();
     }
     offset(e) {
         this.Pos.offset(e);
     }
 }
 
-module.exports = { System, ISystem };
+module.exports = System;
 
 if (require.main === module) {
-    const system = new System(new Pos2d(1, 0), "Test System", {
-        apprearance: "Test Apprearance",
-        size: 2,
-        color: ENUM_SYSTEM_COLOR.PINK,
-    });
-
+    const system = new System(
+        new Pos2d(1, 0),
+        "Test System",
+        "Test Apprearance",
+        2,
+        ENUM_SYSTEM_COLOR.PINK // TODO 类型推导有点问题，应该是枚举的值类型
+    );
+    const CLSSystemNameGenerator = require(path.resolve(__dirname, "./SystemNameGenerator.js"));
+    const nameGenerator = new CLSSystemNameGenerator();
     console.log(system);
     const system2 = new System({ Pos: new Pos2d(0, 1) });
     console.log(system2);
     console.log(system.getDistance(system2));
     system.offset(1);
-    console.log(system);
+    nameGenerator.use_name_template("systemNameRule.json").then(() => {
+        nameGenerator.generate(10).then((names) => {
+            system.randomizeName(nameGenerator)
+            console.log(system);
+        });
+    });
 }
