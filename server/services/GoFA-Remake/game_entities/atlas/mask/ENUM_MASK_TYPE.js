f515be2dc8be52d54a9b0f8f240abb4b3471648b
ClementLevi <1090708360@qq.com>
2025-08-24 21:55:09 +0800

    Freeze ENUM_MASK_TYPE object to prevent modification

--- server/services/GoFA-Remake/game_entities/atlas/mask/ENUM_MASK_TYPE.js
+++ server/services/GoFA-Remake/game_entities/atlas/mask/ENUM_MASK_TYPE.js
@@ -2,13 +2,13 @@
  * @enum {Number}
  * @readonly
  */
-const ENUM_MASK_TYPE = {
+const ENUM_MASK_TYPE = Object.freeze({
     UNDEFINED: 0,
     COLOR: 1,
     RSS_ABUNDANCE: 2,
     POS_OFFSET: 3,
     CAPACITY: 4,
     EXISTENCE: 5,
-};
+});
 
 module.exports = ENUM_MASK_TYPE;
