c97c9b6c110dfbbb07505b6fdd6bce0ba06c3049
ClementLevi <1090708360@qq.com>
2025-10-10 23:25:19 +0800

    feat(server): 添加了一系列类型定义和类型体操
    添加了一系列类型定义和类型体操，用于在JavaScript中进行更严格的类型检查。这些类型定义主要集中在提高对正则表达式、颜色字符串以及其他字符串操作的精确性上。
    
    - 添加了`RegexpType`类型模板，用于将正则表达式类型与字符串类型结合。虽然目前被标记为过时且仅用于JsDoc，但有助于未来扩展。
    - 添加了`Enumerate`类型，用于生成从0到指定数字的枚举类型。
    - 添加了`MakeTuple`类型，用于构建一个指定长度的元组类型，其中元素类型为`any`。
    - 添加了`Add`类型，用于计算两个数字类型的和。
    - 添加了`SliceLast`类型，用于删除元组类型的最后一个元素，并返回删除后的元组类型。
    - 添加了`IsEqualLen`类型，用于判断两个元组类型的长度是否相等。
    - 添加了`CompareTuple`类型，通过递归比较两个元组类型的长度。
    - 添加了`Compare`类型，用于比较两个数字大小。
    - 添加了`Recurrence`类型，用于有限制地递归。
    - 添加了`BuildPowersOf2LengthArrays`类型，用于构建长度为2的幂的数组元组类型。
    - 添加了`ConcatLargestUntilDone`类型，用于递归连接直到完成。
    - 添加了`Replace`类型，用于将元组中的所有元素类型替换为指定类型。
    - 添加了`TupleOf`类型，用于创建一个指定长度的元组类型，其中元素类型为指定类型。
    - 添加了`RangeOf`类型，用于获取从0到指定数字的整数范围。
    - 添加了`IntRange`类型，用于获取从指定的起始数字到结束数字的整数范围。
    - 添加了`HexCharacter`类型，定义了十六进制颜色字符串中允许的字符。
    - 添加了`CapitalizeString`类型，用于将字符串的首字母大写。
    - 添加了`UncapitalizeString`类型，用于将字符串的首字母小写。
    - 添加了`FirstChar`类型，用于获取字符串的第一个字符。
    - 添加了`StartWith`类型，用于判断字符串是否以指定字符开头。
    - 添加了`EndWith`类型，用于判断字符串是否以指定字符结尾。
    - 添加了`StringToTuple`类型，用于将字符串转换为元组类型。
    - 添加了`Join`类型，用于将元组类型转换为字符串。
    - 添加了`RepeatStringBy`类型，用于重复字符串。
    - 添加了`SplitStringBy`类型，用于将字符串按照指定分隔符分割。
    - 添加了`StringLength`类型，用于计算字符串的长度。
    - 添加了`IncludeChar`类型，用于检索字符串是否包含指定字符。
    - 添加了`ReplaceWith`类型，用于将字符串中的指定子字符串替换为另一个字符串。
    - 添加了`TrimLeft`、`TrimRight`、`Trim`类型，用于去除字符串两侧空白。
    - 添加了`Lengthwise`类型，定义了一个具有`length`属性的对象类型。
    - 添加了`HexColorString`类型，用于验证是否为有效的十六进制颜色字符串（形如`#RRGGBB`或`#RRGGBBAA`），不过只检查了后面是否是六位或八位，但未进一步验证字符的合法性。
    - 提供了`main`函数用于测试这些类型

---
+++ server/services/libs/types/types.js
@@ -0,0 +1,395 @@
+/**
+ * @template {RegExp} P
+ * @typedef {string & {__regexPattern: P}} RegexpType
+ * @deprecated
+ * @package
+ * @description 正则表达式类型，相信JsDoc还没有准备好接受这么复杂的类型
+ */
+
+/**
+ * @author Linxae
+ * @see https://juejin.cn/post/7195923686716899387
+ */
+
+/**
+ * @template {number} N
+ * @template {number[]} [Acc=[]]
+ * @typedef {Acc["length"] extends N
+ * ? Acc[number]
+ * : Enumerate<N, [...Acc, Acc["length"]]>} Enumerate
+ * @description 枚举类型
+ * @exports
+ */
+
+/**
+ * @template {number} Target
+ * @template {any[]} [CurTuple=[]]
+ * @typedef {CurTuple["length"] extends Target
+ * ? CurTuple
+ * :MakeTuple<Target, [...CurTuple, any]>} MakeTuple
+ * @description 构建一个长度为Target的元组类型
+ * @package
+ */
+
+/**
+ * @template {number} T1
+ * @template {number} T2
+ * @typedef {[ ...MakeTuple<T1>, ...MakeTuple<T2>]["length"]} Add
+ * @description 两数相加类型
+ * @exports
+ */
+
+/**
+ * @template {number} T
+ * @template {number} F
+ * @typedef {Exclude<Enumerate<Add<T,1> & number>, Enumerate<F>>} _IntRange
+ * @description 整数范围，不好用，占用太高
+ * @package
+ */
+
+/**
+ * @template {any[]} T
+ * @typedef {T extends [...infer Rest, any] ? [...Rest] : []} SliceLast
+ * @description 删除元组类型最后一个元素,返回删除后的元组类型
+ * @exports
+ */
+
+/**
+ * @template {any[]} TA
+ * @template {any[]} TB
+ * @typedef {TA["length"] extends TB["length"]? true : false} IsEqualLen
+ * @description 判断两个元组类型是否长度相等
+ * @exports
+ */
+
+/**
+ * @template {any[]} TA
+ * @template {any[]} TB
+ * @typedef {IsEqualLen<TA, TB> extends false
+ *   ? TA["length"] extends 0
+ *      ? 1
+ *      : TB["length"] extends 0
+ *      ? -1
+ *      : CompareTuple<SliceLast<TA>, SliceLast<TB>>
+ *   : 0} CompareTuple
+ * @description 元组递归比较
+ * @exports
+ */
+
+/**
+ * @template {number} T1
+ * @template {number} T2
+ * @typedef {CompareTuple<MakeTuple<T1>, MakeTuple<T2>>} Compare
+ * @description 比较两数大小
+ * @exports
+ */
+
+/**
+ * @template {any[]} TupleA
+ * @template {any[]} TupleB
+ * @typedef {Compare<TupleA["length"], TupleB["length"]> extends
+ *   | 1
+ *   | 0
+ *   ? TupleB["length"] | Recurrence<TupleA, SliceLast<TupleB>>
+ *   : never} Recurrence
+ * @description 有限制地递归
+ * @exports
+ */
+
+/**
+ * @see https://www.cnblogs.com/gaoshang212/p/15604274.html
+ */
+
+/**
+ * @template {number} N
+ * @template {never[][]} [R=[[never]]]
+ * @typedef {R[0][N] extends never ? R : BuildPowersOf2LengthArrays<N, [[...R[0], ...R[0]], ...R]>} BuildPowersOf2LengthArrays
+ * @description 构建长度为 2 的幂的数组元组类型
+ * @package
+ */
+
+/**
+ * @template {number} N
+ * @template {never[][]} R
+ * @template {never[]} B
+ * @typedef {B["length"] extends N ? B : [...R[0], ...B][N] extends never
+ *   ? ConcatLargestUntilDone<N, R extends [R[0], ...infer U] ? U extends never[][] ? U : never : never, B>
+ *   : ConcatLargestUntilDone<N, R extends [R[0], ...infer U] ? U extends never[][] ? U : never : never, [...R[0], ...B]>} ConcatLargestUntilDone
+ * @description 递归连接直到完成
+ * @package
+ */
+
+/**
+ * @template {any[]} R
+ * @template T
+ * @typedef {{ [K in keyof R]: T }} Replace
+ * @description 替换元组中的元素类型为 T
+ * @package
+ */
+
+/**
+ * @template T
+ * @template {number} N
+ * @typedef {number extends N ? T[] : {
+ *   [K in N]:
+ *   BuildPowersOf2LengthArrays<K, [[never]]> extends infer U ? U extends never[][]
+ *   ? Replace<ConcatLargestUntilDone<K, U, []>, T> : never : never;
+ * }[N]} TupleOf
+ * @description 创建一个长度为 N 的元组类型，元素类型为 T
+ * @exports
+ */
+
+/**
+ * @template {number} N
+ * @typedef {Partial<TupleOf<unknown, N>>["length"]} RangeOf
+ * @description 获取从 0 到 N 的整数范围
+ * @exports
+ */
+
+/**
+ * @template {number} From
+ * @template {number} To
+ * @typedef {Exclude<RangeOf<To>, RangeOf<From>> | From} IntRange
+ * @description 获取从 From 到 To 的整数范围。
+ * @see https://www.cnblogs.com/gaoshang212/p/15604274.html
+ * @borrows _IntRange
+ * @exports
+ */
+
+/**
+ * @typedef {'0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'|'A'|'B'|'C'|'D'|'E'|'F'} HexCharacter
+ * @exports
+ */
+
+/**
+ * //@typedef {`#${HexCharacter}${HexCharacter}${HexCharacter}${HexCharacter}${HexCharacter}${HexCharacter}`} HexColorString
+ * //@deprecated
+ * //@package
+ * //@description 十六进制颜色字符串，对于TS解释器来说也是太多种解释组合，JsDoc还没有准备好接受这么复杂的类型
+ * //@see https://github.com/microsoft/TypeScript/issues/15480
+ */
+
+/** @see https://www.cnblogs.com/echoyya/p/17347552.html#2-%E8%8E%B7%E5%8F%96%E5%AD%97%E7%AC%A6%E4%B8%B2%E7%AC%AC%E4%B8%80%E4%B8%AA%E5%AD%97%E7%AC%A6-firstchar */
+
+/**
+ * @template {string} S
+ * @typedef { S extends string ? `${Capitalize<S>}` : S} CapitalizeString
+ * @description 首字母大写
+ * @exports
+ */
+
+/**
+ * @template {string} S
+ * @typedef { S extends string ? `${Uncapitalize<S>}` : S} UncapitalizeString
+ * @description 首字母小写
+ * @exports
+ */
+
+/**
+ * @template {string} S
+ * @typedef {S extends `${infer L}${infer R}` ? L : never} FirstChar
+ * @description 获取字符串的第一个字符
+ * @exports
+ */
+
+/**
+ * @template {string} S 源字符串
+ * @template {string} C 指定开头字符
+ * @typedef {S extends `${C}${infer R}` ? true : false} StartWith
+ * @description 字符串是否以指定字符开头
+ * @exports
+ */
+
+/**
+ * @template {string} T
+ * @template {string} [F=""]
+ * @typedef {T extends `${infer L}${infer R}` ? LastChar<R, L> : F} LastChar
+ * @description 获取字符串的最后一个字符
+ * @exports
+ */
+
+/**
+ * @template {string} S 源字符串
+ * @template {string} C 指定结尾字符
+ * @typedef {S extends `${infer L}${C}` ? true : false} EndWith
+ * @description 字符串是否以指定字符结尾
+ * @exports
+ */
+
+/**
+ * @template {string} S
+ * @template {any[]} [F=[]]
+ * @typedef { S extends `${infer L}${infer R}` ? StringToTuple<R, [...F, L]> : F} StringToTuple
+ * @description 字符串转元组
+ * @exports
+ */
+
+/**
+ * @template {any[]} T
+ * @template {string} [F=""]
+ * @typedef {T extends [infer L,...infer R]
+ * ? Join<R, `${F}${L & string}`>
+ * : F} Join
+ * @description 元组转字符串
+ * @exports
+ */
+
+/**
+ * @template {string} S 源子字符串
+ * @template {number} C 重复次数
+ * @template {*[]} [A=[]] 拼接过程
+ * @template {string} [B=""] 结果字符串
+ * @typedef {C extends A["length"]
+ *  ? B
+ *  : RepeatStringBy<S, C, [...A, null], `${B}${S}`>} RepeatStringBy
+ * @description 重复字符串
+ * @exports
+ */
+
+/**
+ * @template {string} S 源字符串
+ * @template {string} C 分割符
+ * @template {*[]} [R=[]] 分割结果
+ * @typedef {S extends `${infer Head}${C}${infer Tail}`
+ * ? SplitStringBy<Tail, C , [...R, Head]>
+ * : [...R, S] } SplitStringBy // 最后一次不满足条件时，需要将最后一个单词也放入结果集中
+ * @description 字符串分割
+ * @exports
+ */
+
+/**
+ * @template {string} S
+ * @template {*[]} [F=[]]
+ * @typedef { S extends `${infer L}${infer R}` ? StringLength<R, [...F, L]> : F["length"]} StringLength
+ * @description 字符串长度
+ * @exports
+ */
+
+/**
+ * @template {string} S 源字符串
+ * @template {string} C 待检索字符
+ * @typedef { S extends ""
+ * ? C extends ""
+ *      ? true
+ *      : false   // 空字符串时需要特殊处理
+ * : S extends `${infer L}${C}${infer R}`
+ *      ? true
+ *      : false} IncludeChar
+ * @description 检索字符串是否包含指定字符
+ * @exports
+ */
+
+/**
+ * @template {string} S 源字符串
+ * @template {string} Target 待替换字符串
+ * @template {string} Replacer 替换后字符串
+ * @template {string} [F=""]
+ * @typedef { Target extends ""
+ *   ? S extends ""
+ *       ? Replacer
+ *       : `${Replacer}${S}`
+ *   : S extends `${infer L}${Target}${infer R}` // 匹配模式
+ *       ? ReplaceWith<R, Target, Replacer, `${F}${L}${Replacer}`> // 结果拼接并替换
+ *       : `${F}${S}`} ReplaceWith
+ * @description 字符串替换
+ * @exports
+ */
+
+/**
+ * @template {string} T
+ * @typedef { T extends ` ${infer R}` ? TrimLeft<R> : T} TrimLeft
+ * @description 字符串去除左侧空白
+ * @exports
+ */
+/**
+ * @template {string} T
+ * @typedef { T extends `${infer L} ` ? TrimRight<L> : T} TrimRight
+ * @description 字符串去除右侧空白
+ * @exports
+ */
+/**
+ * @template {string} T
+ * @typedef { TrimLeft<TrimRight<T>>} Trim
+ * @description 字符串去除两侧空白
+ * @exports
+ */
+
+/**
+ * @author Clement_Levi
+ * @description 以下部分才是原创的类型体操
+ */
+
+/**
+ * @typedef  { {length: number} } Lengthwise
+ * @exports
+ */
+
+/**
+ * @template {string} S
+ * @typedef {true extends StartWith<S, '#'>
+ * ? (StringLength<ReplaceWith<S, '#', ''>> extends (6 | 8)
+ *      ? true
+ *      : false)
+ * : false} HexColorString
+ * @description 十六进制颜色字符串，形如 `#RRGGBB` 或 `#RRGGBBAA`
+ * @todo 实际上只检查了后面是不是六位或八位，没管到底是什么字符
+ * @exports
+ */
+
+module.exports = {};
+
+if (require.main === module) {
+    function main() {
+        console.log("This is a library for type checking, not a script.");
+
+        /**@type {Enumerate<3>} */
+        let stepFromThree = 0;
+        /**@type {Enumerate<3>} */
+        let stepFromTwo = 1;
+        /**@type {Enumerate<3>} */
+        let stepFromOne = 2;
+        /**@type {Enumerate<3>} */
+        // @ts-expect-error
+        let dropped = 4;
+
+        /** @typedef {IntRange<0,255>} ColorValue*/
+        /** @type {ColorValue}*/
+        // @ts-expect-error
+        let wrongColorValue = 256;
+        /** @type {ColorValue}*/
+        let okColorValue = 12;
+
+        // @ts-expect-error
+        /** @type {HexColorString} */
+        let okHexColorString = "#FF0000";
+        // @ts-expect-error
+        /** @type {HexColorString} */
+        // @ts-expect-error
+        let badHexColorString = "#GF0000";
+
+        /** @typedef {`#${HexCharacter}${HexCharacter}`} sHexColorString */
+        /** @type {sHexColorString} */
+        // @ts-expect-error
+        let badHexColorString = "#GF";
+
+        // @ts-expect-error
+        /** @type {RegexpType<typeof /^yeah$/>} */
+        // @ts-expect-error
+        let badHexColorString = "#GF";
+        // @ts-expect-error
+        /** @type {RegexpType<typeof /^yeah$/>} */
+        // @ts-expect-error
+        let badHexColorString = "yeah";
+
+        /**
+         * @type {StartWith<"#FF0000", "#">}
+         */
+        let isColor = true;
+
+        /**
+         * @type {HexColorString<"#F0F0000">}
+         */
+        let isNotColor2 = false;
+    }
+    main();
+}
