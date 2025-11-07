9be395d1b6efd25af6fe29faa4dcd3b081d1bf3e
ClementLevi <1090708360@qq.com>
2025-11-08 00:33:46 +0800

    feat(server/main.js): 完善主程序逻辑和初始化流程
    引入Node.js内置模块，优化配置加载和日志记录，增加事件和定时事件注册机制，以及生命周期钩子的实现。
    
    - 使用`node:events`模块增强事件处理能力。
    - 移除已弃用的`dotenv`模块，改为使用命令行参数加载环境变量。
    - 增加对配置项的类型定义和过滤，确保只加载所需环境变量。
    - 修改日志记录方式，增加日志与主程序的绑定。
    - 重构`Main`类，使其继承`EventEmitter`，并增加对事件和定时事件的注册与消费机制。
    - 增加主程序的启动、运行、退出阶段的生命周期钩子。
    - 优化`init`方法，确保在初始化后进入事件循环。
    - 增加`tick`方法，实现固定的tick频率并记录每次tick的耗时，同时检测掉刻情况。
    - 增加`exit`方法，确保在退出时清理资源。
    
    feat(server/services/GoFA-Remake/game.js): 增强游戏逻辑和事件处理机制
    
    - 引入Node.js内置模块，优化事件处理能力。
    - 增加对配置项的类型定义和过滤，确保只加载所需环境变量。
    - 重构`GoFA_Remake_Game`类，使其继承`EventEmitter`，并增加对事件和定时事件的注册与消费机制。
    - 增加游戏的启动、运行、退出阶段的生命周期钩子。
    - 优化`init`方法，确保在初始化后进入运行阶段。
    - 增加`tick`方法，实现游戏逻辑的每次调用，并记录tick的次数。
    - 增加`regEvent`和`registerTimedEvent`方法，实现事件和定时事件的注册。
    - 增加`consumeTimedEvents`方法，实现定时事件的消费。

--- server/main.js
+++ server/main.js
@@ -2,97 +2,458 @@
  * @app entry point of the server.
  */
 // Official modules
-const child_process = require("child_process");
-const path = require("path");
-const dotenv = require("dotenv");
+const path = require("node:path");
+const EventEmitter = require("node:events");
+const child_process = require("node:child_process");
+
+// 3-rd party modules
+///** @deprecated */
+//const dotenv = require("dotenv"); // TODO: use --env .env instead
+//const jsm = require("javascript-state-machine");
+
 // Engine modules
-const Log = require("./services/libs/shared/logger");
+/**
+ * @typedef {InstanceType<import("./services/libs/shared/logger").TypeLogger>} Log
+ * @type {Log}
+ */
+const Log = require(path.resolve(__dirname, "./services/libs/shared/logger"));
+// const { ENUM_DB_TYPE } = require(__dirname + "/services/libs/db/db.type");
+
+/**
+ * @type {import('./services/libs/commands/Commands')}
+ */
+const CONSOLE_COMMANDS = require(path.resolve(
+    __dirname,
+    "./services/libs/commands/Commands"
+));
+
+/**
+ * @typedef {typeof import('./services/libs/shared/DataStructure').Queue} Queue
+ * @typedef {typeof import('./services/libs/shared/DataStructure').Stack} Stack
+ * @type {{Queue: Queue,Stack: Stack}}
+ */
+const { Queue, Stack } = require(path.resolve(
+    __dirname,
+    "./services/libs/shared/DataStructure"
+));
+
 // Game components
-// const {
-//     CMD: COMMAND_LINE_INSTANCE,
-//     registerCommand,
-// } = require("./services/libs/shared/commandLine");
-const CONSOLE_COMMANDS = require("./services/libs/commands/Commands");
-const GAME = require("./services/GoFA-Remake/game");
-// Main Class
-class Main {
-    static ENUM_STAGE = {
-        INIT: "init",
-        RUNNING: "running",
-        EXITING: "exiting",
-    };
-    constructor() {
-        this.stage = Main.ENUM_STAGE.INIT;
-        this.game = null;
-        this.logger = null;
-        this.eventLoopHandle = null;
-        this.config = null;
-    }
+/**
+ * @typedef {typeof import('./services/GoFA-Remake/game')} GameConstructor
+ * @type {GameConstructor}
+ */
+const GameConstructor = require(path.resolve(
+    __dirname,
+    "./services/GoFA-Remake/game"
+));
+/**
+ * @typedef {typeof import('./services/GoFA-Remake/deployments/ServerDeployer')} ServerDeployer
+ * @type {ServerDeployer}
+ */
+const ServerDeployer = require(path.resolve(
+    __dirname,
+    "./services/GoFA-Remake/deployments/ServerDeployer"
+));
+
+/**
+ * @typedef {ReturnType<GameConstructor>} Game
+ * @typedef {import('./services/libs/db/Database')} DatabaseConstructor
+ * @typedef {ReturnType<DatabaseConstructor>} Database
+ * @typedef {import('./services/libs/commands/Commands')} Commands
+ */
+
+/**
+ * @typedef {object} Config
+ * TODO 补充Config类型定义
+ */
+
+/**
+ * @typedef Time
+ * @property {number} TICK - 自启动以来的tick数，即全局时间
+ * @property {number} TICK_RATE - 服务器tick频率, 单位为次/秒
+ * @property {number} TICK_INTERVAL - 服务器tick间隔时间，单位为ms
+ * @property {number} startTime - 服务器启动时间戳
+ * @property {number} last_tick_elapse - 上一次tick耗时
+ * @property {number[]} recentTicksElapses - 最近几次tick耗时
+ * @property {number} recentTicksSize - 最近几次tick队列长度，用以求平均值
+ * @property {number} [LAG_WARN_THRESHOLD=1000/TICK_RATE * 5] - 掉刻警告阈值, 单位为ms
+ */
+
+/**
+ * @exports
+ * @description 主程序接口定义
+ * @typedef IMain
+ * @property {Config?} config - 配置模块实例
+ * @property {InstanceType<Database>?} db - 数据库实例
+ * @property {Game?} game - 游戏实例
+ * @property {Log?} logger - 日志模块实例
+ * @property {ENUM_STAGE[keyof ENUM_STAGE]} stage - 主程序运行阶段枚举
+ * @property {ServerDeployer?} server_deployer - 服务器部署器实例
+ * @property {Time} time - 时间信息
+ * @property {"init"|"running"|"exiting"} ENUM_STAGE - 主程序运行阶段枚举
+ * @property {() => Promise<this>} init - 初始化游戏
+ * @property {() => void} run - 运行游戏
+ * @property {() => void} loadConfig - 加载配置
+ * @property {(logger: Log) => void} regCommands - 注册命令行系统
+ * @property {() => Promise<void>} exit - 退出游戏
+ * @property {() => IMain} getMain - 获取主程序实例
+ */
+
+/**
+ * @typedef {{INIT: "initializing",RUNNING: "running",EXITING: "exiting"}} EnumStage
+ * @readonly
+ * @enum {string}
+ * TODO: 拆到main.d.js中
+ */
+const ENUM_STAGE = Object.freeze({
+    INIT: "initializing",
+    RUNNING: "running",
+    EXITING: "exiting",
+});
+
+/**
+ * @class Main
+ * @implements {IMain}
+ */
+class Main extends EventEmitter {
+    static ENUM_STAGE = ENUM_STAGE;
     /**
-     * 初始化程序
-     * @package
+     * TODO: 补充Main类型定义，应当由.env配置项提供类型定义
+     * @param {*} config
      */
-    async init() {
+    constructor(config) {
+        super();
+        /** @type {Config} */
         // 1. 加载配置读取系统  Load Config Loader
-        this.config = await this.loadConfig();
-        // 2. 加载命令行系统  Load Command Line System
+        this.config = this.loadConfig();
+        /** @type {Database?} */
+        this.db = null;
+        /** @type {ReturnType<setTimeout>?} */
+        this.event_loop_handle = null;
+        /** @type {Game?} */
+        this.game = null;
+        /** @type {Log} */
         this.logger = Log.getInstance();
-        await this.regCommands();
-        // 3. 加载数据库系统  Load Database System
-        this.db = require(path.resolve(__dirname, "./services/libs/db/Database"))(this.config.DB_TYPE, {})
-        // 3.1 数据库内需有合法的
-        // 4. 创建游戏实例  Create Game Instance
-        this.game = GAME.getInstance();
-        await this.game.init();
+        this.logger.bindParent(this);
+        /** @type {EnumStage[keyof EnumStage]} */
+        this.stage = Main.ENUM_STAGE.INIT;
+        /** @type {InstanceType<ServerDeployer>?} */
+        this.server_deployer = null;
+        /**
+         * @type {Time}
+         */
+        this.time = {
+            TICK: 0,
+            TICK_RATE: this.config?.TICK_RATE ?? 5,
+            startTime: 0,
+            recentTicksElapses: new Queue(),
+            recentTicksSize: 5,
+            last_tick_elapse: -1,
+            get TICK_INTERVAL() {
+                return 1000 / (this.TICK_RATE ?? 5);
+            },
+            get LAG_WARN_THRESHOLD() {
+                return this.TICK_INTERVAL * 5;
+            },
+        };
+        /**
+         * @description 事件和待更新任务队列
+         * @type {{onStart:InstanceType<Queue>,
+         * events:Record<string,InstanceType<Queue>>,
+         * timedEvents:Record<number,InstanceType<Queue>>,
+         * cleanups: InstanceType<Queue>,
+         * updatable: InstanceType<Queue>}}
+         */
+        this.ev_queues = {
+            onStart: new Queue(),
+            events: {},
+            timedEvents: {},
+            cleanups: new Queue(), // alias: onExit
+            updatable: new Queue(),
+        };
     }
     /**
-     * 加载配置
-     * @private
+     * @description 加载配置，会过滤掉不需要的env变量
+     * @package
+     * @returns {Config}
      */
-    async loadConfig() {
-        
-        process.env.DOTENV_CONFIG_PATH = path.resolve(".env");
-        const CONFIG = dotenv.config({
-            path: process.env.DOTENV_CONFIG_PATH,
-        })?.parsed;
-        if (CONFIG === undefined || CONFIG?.error) {
-            throw dotenv.config({ path: process.env.DOTENV_CONFIG_PATH }).error;
+    loadConfig() {
+        // TODO: 使用zod库校验配置项，注明哪些项是必填项
+        // 定义我们需要的环境变量键名
+        const neededKeys = {
+            GAME_VERSION: "string",
+            SERVER_NAME: "string",
+            SERVER_PORT: "number",
+            IS_TEST_SERVER: "boolean",
+            TICK_RATE: "number",
+            DB_TYPE: "string",
+            DB_URL: "string",
+            DB_PORT: "number",
+            DB_USERNAME: "string",
+            DB_PASSWORD: "string",
+            MAP_SEED: "string",
+            MAP_SIZE_W: "number",
+            MAP_SIZE_H: "number",
+            MAP_MASK_SCRIPT: "string",
+        };
+        // 创建纯净的配置对象
+        const cleanConfig = {};
+        // 只提取需要的变量并进行基本类型转换
+        for (const key of Object.keys(neededKeys)) {
+            if (process.env[key] !== undefined) {
+                let r = process.env[key];
+                let ret;
+                // @ts-ignore
+                switch (neededKeys[key]) {
+                    case "string":
+                        ret = r;
+                        break;
+                    case "number":
+                        ret = Number(r);
+                        break;
+                    case "boolean":
+                        ret = r === "true";
+                        break;
+                    default:
+                        // @ts-ignore
+                        throw new Error(`Unsupported type: ${neededKeys[key]}`);
+                }
+                // @ts-ignore
+                cleanConfig[key] = ret;
+            }
         }
-        if (!CONFIG) {
-            throw new Error("Failed to load config file. Now exiting...");
-        }
-        this.config = CONFIG;
-        return CONFIG;
-        this.stage = Main.ENUM_STAGE.RUNNING;
+        return cleanConfig;
+    }
+    get TICK_INTERVAL() {
+        return 1000 / this.time.TICK_RATE;
+    }
+    get GAME_LIFE() {
+        return this.game?._gamelife ?? -1;
+    }
+    get OPTIONS() {
+        return this.config ?? {};
     }
     /**
      * 注册命令行系统
      * @private
+     * @param {*} logger - 日志记录器实例
      */
-    async regCommands() {
-        Log.info("Loading command line system...");
+    regCommands(logger) {
+        if (!this.logger) this.logger = logger;
+        // Log.info("Loading command line system...");
         this.logger.registerCommandsBatch(CONSOLE_COMMANDS);
         Log.info("Command line system loaded.");
+    }
+    /**
+     * 注册事件
+     * @param {string | symbol} name
+     * @param {(...args: any[]) => void} cb
+     */
+    regEvent(name, cb) {
+        if (typeof name !== "string") return this;
+        if (typeof cb !== "function") return this;
+        this.on(name, cb);
+        return this;
+    }
+    /**
+     * @description 注册定时事件，在delay个tick后执行
+     * @param {string} name
+     * @param {() => void} cb
+     * @param {number} delay
+     */
+    registerTimedEvent(name, cb, delay) {
+        if (typeof name !== "string") return this;
+        if (typeof cb !== "function") return this;
+        if (typeof delay !== "number") return this;
+        let timeout = this.time.TICK + delay;
+        if (!this.ev_queues.timedEvents[timeout]) {
+            this.ev_queues.timedEvents[timeout] = new Queue();
+        }
+        this.ev_queues.timedEvents[timeout].push(cb);
+        console.log(`Registered Timed Event: ${name} after ${delay} ticks.`);
+        return this;
+    }
+    /**
+     * @description 消费定时事件
+     * @private
+     */
+    consumeTimedEvents() {
+        if (this.ev_queues.timedEvents[this.time.TICK]) {
+            this.ev_queues.timedEvents[this.time.TICK].forEach((task) =>
+                task()  // TODO: 会有异步回调吗
+            );
+            delete this.ev_queues.timedEvents[this.time.TICK];
+        }
+        return this;
+    }
+    /**
+     * @description 刷新最近几次tick耗时队列
+     * @param {number} last_elapse
+     */
+    refreshRecentTicksElapses(last_elapse) {
+        if (this.time.recentTicksElapses.length >= this.time.recentTicksSize) {
+            this.time.recentTicksElapses.shift();
+        }
+        this.time.recentTicksElapses.push(last_elapse);
+        return this;
+    }
+    /**
+     * @description 检测掉刻。注意它只是检测，并不做任何事
+     * @returns {[boolean, number]} - [是否掉刻，平均掉刻耗时]
+     */
+    detectLag() {
+        if (!this.time.LAG_WARN_THRESHOLD) return [false, -1];
+        let average_elapse =
+            this.time.recentTicksElapses.reduce((acc, cur) => acc + cur, 0) /
+            this.time.recentTicksElapses.length;
+        return [average_elapse > this.time.LAG_WARN_THRESHOLD, average_elapse];
+    }
+    deploy() {}
+    // 生命周期阶段 Life Cycle Stages
+    /**
+     * 初始化程序
+     * @package
+     * @returns {Promise<this>}
+     */
+    async init() {
+        // 0. 进入启动状态
+        this.stage = Main.ENUM_STAGE.INIT;
+        // 1. 加载配置  Load Config
+        // 2. 加载命令行系统  Load Command Line System
+        this.logger.initTerminal();
+        this.regCommands(this.logger);
+        // 3. 加载数据库系统  Load Database System
+        /** @type {DatabaseConstructor} */
+        let DBConstructor = require(path.resolve(
+            __dirname,
+            "./services/libs/db/Database"
+        ));
+        /**
+         * @typedef {import("./services/libs/db/DB_Config.d")} DB_Config
+         */
+        this.db = DBConstructor(  // new 关键字已经包含在构造工厂函数内部
+            // TODO: config参数不仅仅是DB的config，需要全面设计IMain的config参数类型
+            this.config?.DB_TYPE ?? "sqlite3",
+            /** @type {DB_Config} */
+            { filePath: this.config?.DB_URL ?? ":memory:" }
+        );
+        this.db.bindParent(this);
+        await this.db.init();
+        // 3.1 数据库内需有合法的数据库和表
+        this.server_deployer = new ServerDeployer(this);
+        this.server_deployer.init();
+        if (!this.server_deployer.validateExistingData()) {
+            this.server_deployer.deploy();
+        }else{
+            this.server_deployer = null;
+        }
+        // 4. 创建游戏实例  Create Game Instance
+        this.game = GameConstructor(this);
+        await this.game.init();
+
+        // END 进入事件循环
+        this.run();
+        return this;
     }
     /**
      * 运行游戏
      * @package
      */
-    async run() {
-        this.eventLoopHandle = setInterval(
-            this.game.tick.bind(this.game),
-            1000 / Number(this.config?.TICK_RATE || 5)
+    run() {
+        this.stage = Main.ENUM_STAGE.RUNNING;
+        this.onStarted();
+        // 进入事件循环
+        this.tick(0);
+    }
+    /**
+     * @param {number} deltaTime
+     */
+    tick(deltaTime) {
+        // 0x00 start measuring this tick duration
+        const T = new Date().getTime();
+        this.time.TICK++;
+        // 0x01 calculate last tick duration to ensure fixed tick rate
+        // if last tick takes shorter than expected, wait for the remaining time.
+        // if last tick takes longer than expected, execute the next tick asap to try to catch up.
+        let next_tick_after = Math.max(
+            this.time.TICK_INTERVAL - this.time.last_tick_elapse,
+            0
+        );
+
+        // 0x02 consume timed events
+        this.consumeTimedEvents();
+
+        // 0x03 update engine logic
+        if (this.ev_queues.updatable[this.time.TICK]) {
+            this.ev_queues.updatable[this.time.TICK].forEach((task) => task());
+        }
+
+        // 0x04 execute game logic
+        this.game ? this.game.tick(deltaTime) : null;
+
+        // 0x05 schedule next tick
+        if (this.stage === Main.ENUM_STAGE.EXITING) return this.onExit();
+        this.event_loop_handle = setTimeout(
+            // this.tick.bind(this),
+            () => {
+                this.tick(next_tick_after);
+            },
+            deltaTime
+        );
+
+        // EOF end of measuring this tick duration
+        this.last_tick_elapse = new Date().getTime() - T;
+        this.refreshRecentTicksElapses(this.last_tick_elapse);
+        // Detect lag
+        if (this.detectLag()[0]) this.onLag();
+    }
+    exit() {
+        this.stage = Main.ENUM_STAGE.EXITING;
+        this.onExit();
+        this.db?.close();
+        return this.event_loop_handle
+            ? clearTimeout(this.event_loop_handle)
+            : -1;
+    }
+    // 生命周期钩子  Life Cycle Hooks
+    /**
+     * @description 开始事件
+     * @private
+     */
+    onStarted() {
+        Log.info(`Server started at ${new Date().toLocaleString()}`);
+        this.ev_queues.onStart.forEach((cb) => cb());
+    }
+    /**
+     * @description 掉刻时执行
+     */
+    onLag() {
+        Log.warn(
+            `Lag detected! Avg tick duration: ${
+                this.detectLag()[1]
+            }ms for recent ${this.time.recentTicksElapses.length} ticks.`
         );
     }
-    getMain() {
-        return this;
+    /**
+     * @description 退出事件
+     */
+    onExit() {
+        this.ev_queues.cleanups.forEach((cb) => cb());
     }
 }
 // ENTRY POINT  程序入口
 if (require.main === module) {
+    process.on("exit", function ExitHandler() {});
+    const MAIN = new Main();
+    MAIN.init()
+        .then((result) => {
+            // result.run();
+        })
+        .catch((err) => {
+            Log.error(err.message);
+            Log.error(err.stack);
+        });
     (async () => {
-        const MAIN = new Main();
-        await MAIN.init();
-        await MAIN.run();
+        Log.debug(MAIN.OPTIONS);
     })();
 }
--- server/services/GoFA-Remake/game.js
+++ server/services/GoFA-Remake/game.js
@@ -1,44 +1,165 @@
-const IGame = require(__dirname + "/../libs/IGame");
-const Log = require(__dirname + "/../libs/shared/logger");
-const singleton = require(__dirname + "/../libs/shared/Singleton");
+/**
+ * @module game
+ */
 
-class GoFA_Remake_Game extends IGame {
-    constructor() {
+const path = require("node:path");
+const EventEmitter = require("node:events");
+
+/**
+ * @typedef {import ("../libs/shared/logger")} Logger
+ * @type {Logger}
+ */
+const Log = require(path.resolve(__dirname, "../libs/shared/logger"));
+
+/**
+ * @template {new (...args: any[]) => T} T
+ * @typedef {typeof import("../libs/shared/Singleton")} Singleton<T>
+ * @type {Singleton<T>}
+ */
+const singleton = require(path.resolve(__dirname, "../libs/shared/Singleton"));
+
+/**
+ * @typedef {typeof import('../libs/shared/DataStructure').Queue} Queue
+ * @typedef {typeof import('../libs/shared/DataStructure').Stack} Stack
+ * @type {{Queue: Queue,Stack: Stack}}
+ */
+const { Queue, Stack } = require(path.resolve(
+    __dirname,
+    "../libs/shared/DataStructure"
+));
+
+/**
+ * @typedef {import ("../../main").IMain} IMain
+ * @typedef {import ("../../main").EnumStage} ENUM_STAGE
+ * @typedef {import ("../libs/game_entities/IGameEntity.d")} IGameEntity
+ *
+ */
+
+/**
+ * @typedef IGame
+ * @property {IMain} env - the environment of the game.
+ * @property {ENUM_STAGE} stage - the current state of the game.
+ * @property {number} tickInterval - the interval of the game's tick.
+ * @property {()=>InstanceType<IGame>} getInstance - get the instance of the game.
+ * @property {()=>void} init - initialize the game.
+ * @property {()=>void} exit - called when the game is exited.
+ * @property {()=>void} beforeTick - called before each tick.
+ * @property {()=>void} tick - called every tick.
+ */
+
+/**
+ * @class GoFA_Remake_Game
+ * @implements {IGame}
+ */
+class GoFA_Remake_Game extends EventEmitter{
+    /**
+     * @member
+     * @static
+     * @readonly
+     */
+    static stages = Object.freeze({
+        INIT: "init",
+        RUNNING: "running",
+        EXITING: "exiting",
+    });
+    /**
+     * @param {IMain} env
+     */
+    constructor(env) {
         super();
+        this.env = env ?? null;
+        this.stage = null;
+        this.tickInterval = -1;
+        /**
+         * @description 事件和待更新任务队列
+         * @type {{onStart:InstanceType<Queue>,
+         * events:Record<string,InstanceType<Queue>>,
+         * timedEvents:Record<number,InstanceType<Queue>>,
+         * cleanups: InstanceType<Queue>,
+         * updatable: InstanceType<Queue>}}
+         */
+        this.ev_queues = {
+            onStart: new Queue(),
+            events: {},
+            timedEvents: {},
+            cleanups: new Queue(), // alias: onExit
+            updatable: new Queue(),
+        };
         this._gamelife = 0;
     }
     /**
-     * @override
+     * @public
      */
     async init() {
+        this.stage = GoFA_Remake_Game.stages.INIT;
         Log.info("Initializing game...");
-        Log.startSpinner("Loading");
-        this.state = IGame.stages.INIT;
-        super.init();
-        let that = this;
-        setTimeout(() => {
-            Log.info("Game initialized.");
-            Log.stopSpinner();
-            that.state = IGame.stages.RUNNING;
-        }, 5000);
+        Log.info("Game initialized.");
+        this.stage = this.env?.stage.RUNNING;
     }
     /**
-     * @override
+     * @public
      */
-    async tick() {
-        switch (this.state) {
-            case IGame.stages.RUNNING:
-                break;
-            case IGame.stages.INIT:
-            case IGame.stages.EXITING:
-                return;
-            default:
-                Log.error(`Unknown game state: ${this.state}`);
-                return;
-        }
+    tick() {
         this._gamelife++;
         Log.info("Game ticked.", this._gamelife);
     }
+    /**
+     * 注册事件
+     * @param {string | symbol} name
+     * @param {(...args: any[]) => void} cb
+     */
+    regEvent(name, cb) {
+        if (typeof name !== "string") return this;
+        if (typeof cb !== "function") return this;
+        this.on(name, cb);
+        return this;
+    }
+    /**
+     * @description 注册定时事件，在delay个tick后执行
+     * @param {string} name
+     * @param {() => void} cb
+     * @param {number} delay
+     */
+    registerTimedEvent(name, cb, delay) {
+        if (typeof name !== "string") return this;
+        if (typeof cb !== "function") return this;
+        if (typeof delay !== "number") return this;
+        let timeout = this._gamelife + delay;
+        if (!this.ev_queues.timedEvents[timeout]) {
+            this.ev_queues.timedEvents[timeout] = new Queue();
+        }
+        this.ev_queues.timedEvents[timeout].push(cb);
+        console.log(`Registered Timed Event: ${name} after ${delay} ticks.`);
+        return this;
+    }
+    /**
+     * @description 消费定时事件
+     * @private
+     */
+    consumeTimedEvents() {
+        if (this.ev_queues.timedEvents[this._gamelife]) {
+            this.ev_queues.timedEvents[this._gamelife].forEach((task) =>
+                task()
+            );
+            delete this.ev_queues.timedEvents[this._gamelife];
+        }
+        return this;
+    }
+    // 生命周期阶段 Life Cycle Stages
+    exit() {}
+    beforeTick() {}
+    getInstance() {
+        return this;
+    }
+    get Instance() {
+        return this;
+    }
+    get PARENT() {
+        return this.env;
+    }
 }
-let GoFA_Instance = singleton(new GoFA_Remake_Game());
-module.exports = GoFA_Instance;
+let GoFA_Constructor = (/** @type {any[]} */ ...args) =>
+    singleton(new GoFA_Remake_Game(...args));
+
+// module.exports = GoFA_Constructor;
+module.exports = (/** @type {any} */ ...args) => new GoFA_Remake_Game(...args);
